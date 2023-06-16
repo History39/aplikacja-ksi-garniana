7b2aabe47d4c91d5cb6d8445f98fc953d309960c
History39 <kf83@protonmail.com>
2023-06-16 09:36:49 +0200

    Ważne zmiany ustawienia itd.

--- website/tests.py
+++ website/tests.py
@@ -1,18 +1,20 @@
 from django.test import TestCase
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import Firefox
+from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get(url='http://127.0.0.1:8000/')
 
+
+driver = Firefox(executable_path=GeckoDriverManager().install())
+driver.get(url='http://127.0.0.1:8000/admin')
 
 
 # Create your tests here.
 
-def website_responsiveness(width,file):
+def website_responsiveness(w,file):
     height = 768
-    driver.set_window_size(width,height)
+    driver.set_window_size(w,height)
     driver.save_screenshot(file)
 
-website_responsiveness(600, "test600".png)
+website_responsiveness(600, "test600.png")
 # Create your tests here.
